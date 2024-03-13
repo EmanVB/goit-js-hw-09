cea0065e831da880cdfb0d6f1a92c06d74a7b853
EmanVB <emanbonsato@yahoo.com>
2024-03-13 23:20:05 +0800

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const body = document.querySelector("body");
+const startButton = document.querySelector("button[data-start]");
+const stopButton = document.querySelector("button[data-stop]");
+stopButton.disabled = true;
+let colorChangeInterval = null;
+
+body.style.display = "flex";
+body.style.flexDirection = "row";
+body.style.justifyContent = "center";
+body.style.alignItems = "center";
+body.style.gap = "100px";
+body.style.height = "100vh";
+body.style.width = "auto";
+
+function startColorChange() {
+    colorChangeInterval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        startButton.disabled = true;
+        stopButton.disabled = false;
+    }, 1000);
+}
+
+function stopColorChange() {
+    clearInterval(colorChangeInterval);
+    startButton.disabled = false;
+    stopButton.disabled = true; 
+}
+
+startButton.addEventListener("click", startColorChange);
+stopButton.addEventListener("click", stopColorChange);
